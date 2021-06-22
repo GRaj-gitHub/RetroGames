e571c8fca67e461f055f2afd127d43fc363837ce
GRaj-gitHub <rajgopika1105@gmail.com>
2021-06-22 14:51:01 +0100

    edited

--- Chess/main.cpp
+++ Chess/main.cpp
@@ -1,10 +1,10 @@
-
+#include "GameObjects.h"
 
 int main()
 {
    sf::RenderWindow window(sf::VideoMode(512, 512), "Chess");
    
-   /*sf::Texture boardTex;
+   sf::Texture boardTex;
     boardTex.loadFromFile("../Assets/Images/Chess/Board.png");
   
     sf::Sprite BgSprite;
@@ -12,12 +12,15 @@ int main()
    
     sf::Texture pieceTex;
     sf::Sprite B_pieces[6];
-    for (int i = 0; i < 6; ++i)
-    {
-        pieceTex.loadFromFile("../Assets/Images/Chess/pieces.png", sf::IntRect(0, 0, 64 * (i + 1), 64 * (i + 1)));
-        B_pieces[i].setTexture(pieceTex);
-        B_pieces[i].setPosition(sf::Vector2f(64 * (i + 1), 64));
-    }*/
+   // for (int i = 0; i < 6; i++)
+  //  {
+        pieceTex.loadFromFile("../Assets/Images/Chess/pieces.png", sf::IntRect( 64 * 0 , 64 * 0, 64, 64));
+       
+        B_pieces[0].setTexture(pieceTex);
+        pieceTex.loadFromFile("../Assets/Images/Chess/pieces.png", sf::IntRect(64 * 0, 64 * 0, 64, 64));
+        B_pieces[1].setTexture(pieceTex);
+        B_pieces[1].setPosition(sf::Vector2f(64 , 0));
+    //}
   
 
     while (window.isOpen())
@@ -29,13 +32,13 @@ int main()
                 window.close();
         }
 
-        window.clear();
+        window.clear(sf::Color::Black);
         window.draw(BgSprite);
-        for (int i = 0; i < 6; i++)
-        {
+       for (int i = 0; i < 2; i++)
+       {
             window.draw(B_pieces[i]);
-        }
-       
+       }
+        //window.draw(B_pieces[1]);
         window.display();
     }
 
